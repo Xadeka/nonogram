bc70b7db5a9c7900fdc107557d416000dd89d46b
Xadeka <me@zade.dev>
2021-03-07 18:43:10 -0500

    Setting tabIndex of cells in JSX instead of effect

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -31,30 +31,16 @@ export const Grid = ({ rows, onCellChange }: GridProps) => {
     row: 0,
   });
 
-  // When active cell changes:
-  // - Update the roving tab index. Next cell gets 1; Previous cell gets -1.
-  // - Change focus to the next cell.
-  // Reference: https://www.w3.org/TR/wai-aria-practices/#kbd_roving_tabindex
+  let focusedCellId = getCellId(focusedPosition);
+
+  // When active cell changes, focus that element.
   useEffect(() => {
-    let elementReceivingFocus = document.getElementById(
-      getCellId(focusedPosition)
-    );
+    let elementReceivingFocus = document.getElementById(focusedCellId);
 
     if (elementReceivingFocus !== null) {
-      elementReceivingFocus.tabIndex = 0;
       elementReceivingFocus.focus();
     }
-
-    // During effect clean up, the last element receiving focus will lose focus.
-    // Creating new variable for clarity.
-    let elementLosingFocus = elementReceivingFocus;
-
-    return () => {
-      if (elementLosingFocus !== null) {
-        elementLosingFocus.tabIndex = -1;
-      }
-    };
-  }, [focusedPosition]);
+  }, [focusedCellId]);
 
   // Calculate height and width for convience in handleCellKeyDown.
   let height = rows.length;
@@ -133,29 +119,42 @@ export const Grid = ({ rows, onCellChange }: GridProps) => {
           return (
             // TODO: Investigate a better key for rows and cells.
             <tr key={rowIndex} role="row">
-              {row.map((cellValue, columnIndex) => (
-                <td key={columnIndex} role="gridcell" style={{ lineHeight: 0 }}>
-                  <button
-                    id={getCellId({ column: columnIndex, row: rowIndex })}
-                    className="rounded border border-green-500 h-5 w-5 p-0"
-                    onClick={(e) => onCellClick(cellValue)}
-                    // Default -1 and will be updated once this element has focus.
-                    tabIndex={-1}
-                    // The button pressed state will represent "filled".
-                    // Since we will be controlling these values in state,
-                    //   we will set the aria attribute ourselves.
-                    aria-pressed={cellValue === "filled"}
+              {row.map((cellValue, columnIndex) => {
+                const cellId = getCellId({
+                  column: columnIndex,
+                  row: rowIndex,
+                });
+
+                return (
+                  <td
+                    key={columnIndex}
+                    role="gridcell"
+                    style={{ lineHeight: 0 }}
                   >
-                    {/*
-                     * This is the accessible name for the button.
-                     * Using the button's content instead of aria-label.
-                     * If there isn't CSS on the page, we will still have labels on the buttons.
-                     * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/button_role#basic_buttons
-                     */}
-                    <span className="sr-only">{`${columnIndex}:${rowIndex}`}</span>
-                  </button>
-                </td>
-              ))}
+                    <button
+                      id={getCellId({ column: columnIndex, row: rowIndex })}
+                      className="rounded border border-green-500 h-5 w-5 p-0"
+                      onClick={(e) => onCellClick(cellValue)}
+                      // tabIndex is determined by what cell is currently focused.
+                      // The focused cell will have 1 while all others will have -1.
+                      // Reference: https://www.w3.org/TR/wai-aria-practices/#kbd_roving_tabindex
+                      tabIndex={cellId === focusedCellId ? 1 : -1}
+                      // The button pressed state will represent "filled".
+                      // Since we will be controlling these values in state,
+                      //   we will set the aria attribute ourselves.
+                      aria-pressed={cellValue === "filled"}
+                    >
+                      {/*
+                       * This is the accessible name for the button.
+                       * Using the button's content instead of aria-label.
+                       * If there isn't CSS on the page, we will still have labels on the buttons.
+                       * https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/button_role#basic_buttons
+                       */}
+                      <span className="sr-only">{`${columnIndex}:${rowIndex}`}</span>
+                    </button>
+                  </td>
+                );
+              })}
             </tr>
           );
         })}
